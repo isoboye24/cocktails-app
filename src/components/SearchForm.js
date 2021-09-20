16701e36588dfb0eff05a6f4fd0719a4888b91a8
isoboye24 <isobarguy4real@gmail.com>
2021-09-20 06:15:35 +0300

    A complete cocktail app.
    This is to showcase a search engine and also single page description of the item searched and clicked.

---
+++ src/components/SearchForm.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react";
+import { useGlobalContext } from "../context";
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  // To set the input focus
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  // This is to prevent reload of the page
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search your favourite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt ipsam
+        nisi illo, veritatis error corporis tempora odio porro totam maiores
+        eligendi hic saepe accusantium animi ad omnis commodi quasi at! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Quam iste beatae ex
+        sunt nihil enim dolor possimus vel iure atque autem illum quia fuga
+        dicta, blanditiis perferendis quae consequuntur tenetur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
